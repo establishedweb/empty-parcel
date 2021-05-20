5096e6ca3b6fc09f4fa1aabcb3bd81a1269a3e31
Josh Broughton <establishedwebsolutions@gmail.com>
2021-05-20 09:14:52 -0700

    Updated links in footer

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -11,24 +12,18 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'empty-parcel' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'empty-parcel' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'empty-parcel' ), 'empty-parcel', '<a href="https://automattic.com/">Automattic</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+<footer id="colophon" class="site-footer">
+	<div class="site-info">
+		<?php
+		/* translators: 1: Theme name, 2: Theme author. */
+		printf(esc_html__('Theme: %1$s by %2$s.', 'empty-parcel'), '<a href="https://github.com/establishedweb/empty-parcel">empty-parcel</a>', '<a href="https://establishedweb.com/">Established Web</a>');
+		?>
+	</div><!-- .site-info -->
+</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
